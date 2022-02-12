5293d990e3c66c80e0110027b8097e19d3a1dbb8
Stephen Ocaya <socaya@stephocay.com>
2022-02-12 07:26:44 +0300

    Added ACtivity Details LAyout

--- src/components/ActivityDetails.js
+++ src/components/ActivityDetails.js
@@ -1,10 +1,20 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Stack, Heading, Flex } from "@chakra-ui/react";
 
-const ActivityDetails = () =>{
+const ActivityDetails = () => {
     return (
-        <Box>
-            Group Activity Details:
-        </Box>
+        <Stack>
+            <Flex alignItems="center" justifyContent="left" w="100%" pl={15} bg="gray.100">
+                <Heading as="h4" size="lg" color="blackAlpha.600">
+                    ICYD Group Activity Tool
+                </Heading>
+            </Flex>
+            <Stack>
+                <Flex>
+                    <Box w="20%" h=""> SSS</Box>
+                    <Box w="80%" bg="yellow.200"> BB</Box>
+                </Flex>
+            </Stack>
+        </Stack>
     )
 }
 
