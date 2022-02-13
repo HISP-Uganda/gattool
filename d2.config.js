fdd0f37654413b9fb0eb296a57525f6b324ae22f
Charles Olupot <olupotcharles@gmail.com>
2022-02-13 20:20:33 +0300

    Changes

--- d2.config.js
+++ d2.config.js
@@ -1,12 +1,11 @@
 const config = {
-    type: 'app',
-    name:'gat-tool',
-    title:'Group Activity Tool',
-    description:'A Simplified Tracker capture tool for managing Group Activities for the ICYD Project',
-    author:'HISP Uganda',
-    entryPoints: {
-        app: './src/AppWrapper.js',
-    },
-}
+  type: "app",
+  name: "gat-tool",
+  title: "Group Activity Tool",
+  description: "A Simplified Tracker capture tool",
+  entryPoints: {
+    app: "./src/AppWrapper.js",
+  },
+};
 
-module.exports = config
+module.exports = config;
